126ed497f9c243fbaf133e90951f168aa1d765cc
greynix <greynix@gmail.com>
2017-01-22 22:19:31 +0300

    add scripts and readme

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+This script takes as an argument text file and show top-10 reps words.
+
+
+# Quickstart
+
+Run the script with the file as an argument.
+
+Example of script launch on Linux, Python 3.5:
+
+```#!bash
+
+$ python lang_frequency.py <path to file>
+
+[('и', 10200), ('в', 5285), ('не', 4383), ('что', 3698), ('на', 3373), ('он', 3178), ('с', 3087), ('как', 2075), ('я', 1836), ('к', 1799)]
+
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,32 @@
-
+import argparse
 
 def load_data(filepath):
-    pass
+    our_text = open(filepath)
+    return our_text
 
+words = {} #Dictionary for the word count {'word':number}
 
 def get_most_frequent_words(text):
-    pass
+    """
+    Split each line into words, modify and
+    add to dictionary as a key and count of reps as a value.
+    """
+    for i in text.readlines():
+        i = i.strip().lower()
+
+        for char in ['*','-','–','"']:
+            i = i.replace(char,'')
+
+        for a in i.split():
+            words[a] = words.get(a,0) + 1
+    text.close()
 
 
 if __name__ == '__main__':
-    pass
+    parser=argparse.ArgumentParser(description='''Show frequent words.''')
+    parser.add_argument('text_file', type=str, help='filepath')
+
+    user_input = parser.parse_args()
+    get_most_frequent_words(load_data(user_input.text_file))
+    print(sorted(words.items(), key=lambda a : a[1],reverse=True)[:10])
+
